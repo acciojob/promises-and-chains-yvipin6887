aef1417aa753ca88ee10ed8b6878415e4cba9c96
Vipin Yadav <76983380+yvipin6887@users.noreply.github.com>
2025-05-03 12:34:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("userForm");
+	
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const age = document.getElementById("age").value.trim();
+    const name = document.getElementById("name").value.trim();
+	  if (!age || !name) {
+		  
+		alert('Please enter valid details.')
+
+		  return;
+	  }
+
+	  new Promise((resolve) => {
+		  setTimeout(() => resolve(), 4000)
+	  }). then(() => {
+		  if (age > 18) {
+			  alert(`Welcome, ${name}. You can vote.`);
+		  } else {
+			  alert(`Oh sorry, ${name}. You aren't old enough.`);
+		  }
+	  })
+
+    
+  });
+});
